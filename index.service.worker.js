09b4a3e041eb82bea8ff90a082abcfb1e8d95286
trekfan42 <davisam42@gmail.com>
2023-01-25 13:06:04 -0500

    Lander 42 v3
    Options menu settings added:
    -invert controls
    -volume adjustments

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674624364|96858638";
+const CACHE_VERSION = "1674669927|6582262703";
 const CACHE_PREFIX = "Lander 42-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
