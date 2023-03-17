383ce38026a59c5e737c28db0ed7408de2346c42
trekfan42 <davisam42@gmail.com>
2023-03-17 17:16:54 -0400

    v0.2.2
    crt effect toggle added

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679087551|1410561403";
+const CACHE_VERSION = "1679087797|1656654977";
 const CACHE_PREFIX = "Lander 42-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
