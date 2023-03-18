c6d1086c0b4191fd89dd8048b38e5eee26b9d784
trekfan42 <davisam42@gmail.com>
2023-03-18 10:03:59 -0400

    v0.2.4
    Shop Framework started, Shop Pad added

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1679088571|2429776830";
+const CACHE_VERSION = "1679148215|27911184";
 const CACHE_PREFIX = "Lander 42-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
