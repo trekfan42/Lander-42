780cb345a3c844bd5d88ba8ff0c43e45cc8d40c9
trekfan42 <davisam42@gmail.com>
2023-03-30 12:38:11 -0400

    v0.3.6

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1680136664|24967756283";
+const CACHE_VERSION = "1680194272|13660236758";
 const CACHE_PREFIX = "Lander 42-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
